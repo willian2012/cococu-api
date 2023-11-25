5e86fd4929ec5b382f90340e721c8d2c68dcab8e
William <xebas610@gmail.com>
2023-11-25 11:05:18 -0500

    change in the values in migrations

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -9,19 +9,25 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 191)->unique(); 
+            $table->string('token', 191)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
+
+            // Especifica la longitud máxima del índice compuesto
+            $table->index(['tokenable_type', 'tokenable_id'], 'tokenable_index', ['length' => ['tokenable_type' => 191, 'tokenable_id' => 6]]);
+
             $table->timestamps();
         });
     }
+
+
 
     /**
      * Reverse the migrations.
