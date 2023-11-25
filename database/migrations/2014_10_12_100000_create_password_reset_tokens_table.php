d9bd751adc297807b150e4d91e698f9c1634c403
William <xebas610@gmail.com>
2023-11-25 18:21:37 -0500

    new changes

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,13 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->string('email', 191)->unique(); // Ajusta la longitud según tus necesidades
-            $table->string('token', 191);
-            $table->timestamps(); // Esto añade created_at y updated_at automáticamente
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
-
 
     /**
      * Reverse the migrations.
--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -9,24 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 191)->unique();
+            $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
-
-            // Especifica la longitud máxima de las columnas en el índice compuesto
-            $table->index(['tokenable_type' => 191, 'tokenable_id' => 191], 'personal_access_tokens_type_id_index');
-
             $table->timestamps();
         });
     }
-
 
     /**
      * Reverse the migrations.
